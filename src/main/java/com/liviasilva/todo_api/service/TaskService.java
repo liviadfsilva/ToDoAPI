3d78df1f0591cb7dcb5f6822fc87af61c021a81f
liviadfsilva <liviadfsilva@gmail.com>
2025-10-27 18:38:51 -0300

    feat: create TaskService

---
+++ src/main/java/com/liviasilva/todo_api/service/TaskService.java
@@ -0,0 +1,51 @@
+package com.liviasilva.todo_api.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.liviasilva.todo_api.model.Task;
+import com.liviasilva.todo_api.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Task saveTask(Task task) {
+        return repository.save(task);
+    }
+    
+    public void deleteTask(Long id) {
+        repository.deleteById(id);
+    }
+
+    public List<Task> getImportantAndUrgentTasks() {
+        return repository.findByImportantAndUrgent(true, true);
+    }
+
+    public List<Task> getImportantButNotUrgentTasks() {
+        return repository.findByImportantAndUrgent(true, false);
+    }
+
+    public List<Task> getNotImportantAndUrgentTasks() {
+        return repository.findByImportantAndUrgent(false, true);
+    }
+
+    public List<Task> getNotImportantAndNotUrgentTasks() {
+        return repository.findByImportantAndUrgent(false, false);
+    }
+    
+}
