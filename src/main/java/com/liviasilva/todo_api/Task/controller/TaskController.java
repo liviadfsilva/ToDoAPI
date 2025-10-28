a921773744eeab46bab158be594183ce5406ef47
liviadfsilva <liviadfsilva@gmail.com>
2025-10-28 17:41:23 -0300

    feat: create TaskController

---
+++ src/main/java/com/liviasilva/todo_api/Task/controller/TaskController.java
@@ -0,0 +1,73 @@
+package com.liviasilva.todo_api.Task.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liviasilva.todo_api.Task.model.Task;
+import com.liviasilva.todo_api.Task.service.TaskService;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return service.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return service.getTaskById(id)
+        .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return service.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return service.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        service.deleteTask(id);
+    }
+
+    // Eisenhower filters
+    @GetMapping("/important-urgent")
+    public List<Task> getImportantAndUrgent() {
+        return service.getImportantAndUrgentTasks();
+    }
+
+    @GetMapping("/important-not-urgent")
+    public List<Task> getImportantButNotUrgent() {
+        return service.getImportantButNotUrgentTasks();
+    }
+
+    @GetMapping("/not-important-urgent")
+    public List<Task> getNotImportantButUrgent() {
+        return service.getNotImportantButUrgentTasks();
+    }
+
+    @GetMapping("/not-important-not-urgent")
+    public List<Task> getNotImportantAndNotUrgent() {
+        return service.getNotImportantAndNotUrgentTasks();
+    }
+}
