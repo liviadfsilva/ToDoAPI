1543113e41f089d9bacb100ef42e377e1ea44fbb
liviadfsilva <liviadfsilva@gmail.com>
2025-10-30 16:19:25 -0300

    feat: create user repo

--- src/main/java/com/liviasilva/todo_api/User/repository/UserRepository.java
+++ src/main/java/com/liviasilva/todo_api/User/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.liviasilva.todo_api.User.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.liviasilva.todo_api.User.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     
+    Optional<User> findByEmail(String email);
 }
