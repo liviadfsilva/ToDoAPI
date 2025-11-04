68167d0b0ee35f1131307853f6b84a2befd279ef
liviadfsilva <liviadfsilva@gmail.com>
2025-11-04 16:09:36 -0300

    replace Lombok @Data for @Getter and @Setter due to annotation processor crash

--- src/main/java/com/liviasilva/todo_api/User/model/User.java
+++ src/main/java/com/liviasilva/todo_api/User/model/User.java
@@ -10,14 +10,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -25,7 +28,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
