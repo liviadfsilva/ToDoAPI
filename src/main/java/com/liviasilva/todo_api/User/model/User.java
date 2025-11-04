75bf27f9707974fa7323d40a066a38476c309999
liviadfsilva <liviadfsilva@gmail.com>
2025-11-04 14:40:08 -0300

    feat: create one-to-many relationship with tasks

--- src/main/java/com/liviasilva/todo_api/User/model/User.java
+++ src/main/java/com/liviasilva/todo_api/User/model/User.java
@@ -1,9 +1,17 @@
 package com.liviasilva.todo_api.User.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.liviasilva.todo_api.Task.model.Task;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,5 +36,9 @@ public class User {
 
     @Column(name = "passwordHash", nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Task> tasks = new ArrayList<>();
     
 }
